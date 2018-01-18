50bd19578a674b54acc167add233a31e26637533
K1460846 <k1460846@kingston.ac.uk>
2018-01-18 15:06:46 +0000

    Use env.list for pdo connection

--- src/pdoConnect.php
+++ src/pdoConnect.php
@@ -7,9 +7,17 @@
  */
 
 try {
-    $dsn = 'mysql:host=db;dbname=exampledb';
-    $db = new PDO($dsn, 'dbuser', 'dbpassword');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if(file_exists('../config/env.list')) {
+        $dbDetails = file_get_contents('../config/env.list');
+        $ini = parse_ini_file('../config/env.list');
+        $dbName = $ini['MYSQL_DATABASE'];
+        $dbUser = $ini['MYSQL_USER'];
+        $dbPassword = $ini['MYSQL_PASSWORD'];
+
+        $dsn = "mysql:host=db;dbname=$dbName";
+        $db = new PDO($dsn, "$dbUser", "$dbPassword");
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
 } catch(PDOException $e){
     echo $e->getMessage();
 }
