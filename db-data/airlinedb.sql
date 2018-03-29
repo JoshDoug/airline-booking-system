f551d0c08cc1d92b25b94b1f013289d7e8b82221
K1460846 <k1460846@kingston.ac.uk>
2018-03-29 12:59:05 +0100

    Update db import sql

--- db-data/airlinedb.sql
+++ db-data/airlinedb.sql
@@ -1,30 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Generation Time: Mar 29, 2018 at 11:54 AM
+-- Server version: 10.2.12-MariaDB-10.2.12+maria~jessie
+-- PHP Version: 7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `airlinedb`
 --
-USE `airlinedb`;
 
-CREATE TABLE IF NOT EXISTS `customers` (
-  `customerId` INT(7) NOT NULL,
-  `firstName` VARCHAR(30) NOT NULL,
-  `lastName` VARCHAR(30) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `flight` (
-  `flightId` INT(7) NOT NULL,
-  `origin` VARCHAR(30) NOT NULL,
-  `destination` VARCHAR(30) NOT NULL,
-  `departureDateTime` DATETIME,
-  `arrivalDateTime` DATETIME,
-  `distance` INT(7), -- In KM?
-  `passengerCapacity` INT(3)
-);
+--
+-- Table structure for table `administrators`
+--
 
-CREATE TABLE IF NOT EXISTS `bookingReference` (
+CREATE TABLE `administrators` (
+  `adminId` int(10) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `firstName` text NOT NULL,
+  `lastName` text NOT NULL,
+  `companyEmail` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-)
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `customerId` int(7) NOT NULL,
+  `firstName` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `lastName` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locations`
+--
+
+CREATE TABLE `locations` (
+  `location` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `administrators`
+--
+ALTER TABLE `administrators`
+  ADD PRIMARY KEY (`adminId`),
+  ADD UNIQUE KEY `companyEmail` (`companyEmail`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customerId`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `locations`
+--
+ALTER TABLE `locations`
+  ADD PRIMARY KEY (`location`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `administrators`
+--
+ALTER TABLE `administrators`
+  MODIFY `adminId` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customerId` int(7) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
