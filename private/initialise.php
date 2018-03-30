b1f23a45de7e000eb0d8e6fbed80a3f1904e3a28
K1460846 <k1460846@kingston.ac.uk>
2018-03-30 16:01:53 +0100

    Remove constant

--- private/initialise.php
+++ private/initialise.php
@@ -4,7 +4,6 @@ define("PRIVATE_PATH", __DIR__);
 define("WEB_ROOT", dirname(PRIVATE_PATH));
 define("SRC_ROOT", WEB_ROOT . '/src');
 define("CONFIG_PATH", WEB_ROOT . '/config');
-define("CONTROLLER_PATH", PRIVATE_PATH . '/controller');
 define("MODEL_PATH", PRIVATE_PATH . '/model');
 
 // Load Class Definitions
