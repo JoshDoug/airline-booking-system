f369b01c600ce4d7055edc1e1da0632b3709f601
K1460846 <k1460846@kingston.ac.uk>
2018-03-30 20:59:57 +0100

    Add links and flight search form

--- src/admin/manageFlights.php
+++ src/admin/manageFlights.php
@@ -2,18 +2,22 @@
 require_once '../../private/initialise.php';
 
 if(isset($_REQUEST['generateFlights'])) {
-    $dates = addFlights($_REQUEST['endDate']);
+    addFlights($_REQUEST['endDate']);
 }
 
 // Controller logic
+$locations = getLocations();
 
 // View logic to be separated
 // Pagination probably would be useful here...
 
-echo date("Y-m-d");
-echo strtotime('Monday', date("Y-m-d"));
-
 ?>
+
+<h1>Manage Flights</h1>
+
+<p><a href="index.php">Admin Page</a></p>
+
+<h2>Generate Flights</h2>
 <form action="manageFlights.php" method="post">
     <input type="hidden" name="generateFlights" value="generateFlights">
     Set date to generate flights until using the flight types:
@@ -21,14 +25,25 @@ echo strtotime('Monday', date("Y-m-d"));
     <input type="submit" name="Generate Flights">
 </form>
 
-<ul>
-    <li><?=date("Y-m-d")?></li>
-    <li><?=strtotime('Monday', strtotime(date("Y-m-d")))?></li>
-    <li><?=date('Y-m-d', strtotime('Monday', strtotime(date("Y-m-d"))))?></li>
-</ul>
+<h2>View Flights</h2>
 
-<ul>
-    <?php foreach ($dates as $date) : ?>
-    <li><?= $date ?></li>
-    <?php endforeach ?>
-</ul>
+<form action="manageFlightTypes.php" method="get">
+    Search for flights to or from Stansted:
+    <select name="departurePoint">
+        <option value="All">All</option>
+        <?php foreach ($locations as $location): ?>
+            <option value="<?= $location->locationName ?>"><?= $location->locationName ?></option>
+        <?php endforeach ?>
+    </select>
+
+    <select name="destination">
+        <option value="All">All</option>
+        <?php foreach ($locations as $location): ?>
+            <option value="<?= $location->locationName ?>"><?= $location->locationName ?></option>
+        <?php endforeach ?>
+    </select>
+
+    <input type="date" name="endDate" min="<?= date('Y-m-d') ?>">
+    <input type="date" name="endDate" min="<?= date('Y-m-d') ?>">
+    <input type="submit" value="Search Flights"/>
+</form>
