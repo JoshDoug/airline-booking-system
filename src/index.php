2715533b07a753aa410bbc0ac3ab2fc3e4d1a1c7
K1460846 <k1460846@kingston.ac.uk>
2018-04-02 22:23:26 +0100

    Add retrieve booking form, brand name, basic nav

--- src/index.php
+++ src/index.php
@@ -1,17 +1,23 @@
 <?php
 require_once '../private/initialise.php';
-
 $locations = getLocations();
-
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Home</title>
+    <title>Fly Guys</title>
 </head>
 <body>
-<h1>Home</h1>
+<!-- Change this as necessary but keep the links -->
+<nav>
+    <ul>
+        <li><a href="basket.php">Basket</a></li>
+        <li><a href="loginUser.php">Login</a></li>
+        <li><a href="registerUser.php">Register</a></li>
+    </ul>
+</nav>
 
+<h1>Fly Guys</h1>
 <form action="searchResults.php" method="get">
     Search for flights to or from Stansted:
     <select name="departurePoint">
@@ -32,5 +38,32 @@ $locations = getLocations();
     <input type="date" name="endDate" min="<?= date('Y-m-d') ?>">
     <input type="submit" value="Search Flights"/>
 </form>
+<h2>Retrieve Booking</h2>
+<form action="booking.php" method="post">
+    <p>
+        <label for="firstName">First Name:
+            <input type="text" name="firstName" required>
+        </label>
+    </p>
+    <p>
+        <label for="lastName">Last Name:
+            <input type="text" name="lastName" required>
+        </label>
+    </p>
+    <p>
+        <label for="email">Email:
+            <input type="text" name="email" required>
+        </label>
+    </p>
+    <p>
+        <label for="booking">Booking Number:
+            <input type="text" name="email" required>
+        </label>
+    </p>
+    <p>
+        <input type="submit" name="register" value="Retrieve Booking">
+    </p>
+</form>
+<!-- Image Carousel Stuff I guess -->
 </body>
 </html>
