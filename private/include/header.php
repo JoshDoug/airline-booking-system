9f9fbc11abf9890995c7c55c8aef370c26988f0b
K1460846 <k1460846@kingston.ac.uk>
2018-04-05 17:30:59 +0100

    Add auth changes

--- private/include/header.php
+++ private/include/header.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_SESSION['authenticatedUser'])) {
+if (isset($_SESSION['authenticatedUser'])) {
     $customer = getCustomerByEmail($_SESSION['authenticatedUser']);
     $authenticated = true;
 }
@@ -8,15 +8,16 @@ if(isset($_SESSION['authenticatedUser'])) {
     <h1><a href="index.php">Fly Guys</a></h1>
     <!-- Change this as necessary but keep the links -->
     <nav>
-        <?php if($authenticated) : ?>
-        <p><?= $customer->firstName ?></p>
+        <?php if ($authenticated) : ?>
+            <p><?= $customer->firstName ?></p>
         <?php endif ?>
         <ul>
             <li><a href="basket.php">Basket</a></li>
-            <?php if($authenticated) : ?>
+            <?php if ($authenticated) : ?>
+                <li><a href="user.php">Profile</a></li>
                 <li><a href="logout.php">Logout</a></li>
             <?php else : ?>
-            <li><a href="login.php">Login</a></li><!-- Could be removed if we're going to keep the login form -->
+                <li><a href="login.php">Login</a></li><!-- Could be removed if we're going to keep the login form -->
             <?php endif ?>
             <li><a href="register.php">Register</a></li>
         </ul>
