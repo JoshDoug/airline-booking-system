ce3bc45991f9b1183ec7dc097c5eeeac87b0b921
K1460846 <k1460846@kingston.ac.uk>
2018-04-09 21:45:24 +0100

    Add creds just incase

--- db-data/airlinedb.sql
+++ db-data/airlinedb.sql
@@ -41,7 +41,7 @@ CREATE TABLE `administrator` (
 
 --
 -- Dumping data for table `administrator`
---
+-- Default Creds - admin@flyguys.com and password (eek)
 
 INSERT INTO `administrator` (`adminId`, `password`, `firstName`, `lastName`, `companyEmail`) VALUES
   (1, '$2y$10$v9bDoH0ObH4AXEbtUGqydOBan7We24Tp6vQ3oi5QCZvrr228Bm.3G', 'Admin', 'User', 'admin@flyguys.com');
