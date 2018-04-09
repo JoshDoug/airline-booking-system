281d2f22d81914d80bfedec4e6a497a97b95f0be
K1460846 <k1460846@kingston.ac.uk>
2018-04-09 13:20:37 +0100

    Cleanup files

--- private/include/header.php
+++ private/include/header.php
@@ -7,20 +7,23 @@ if (isset($_SESSION['authenticatedUser'])) {
 }
 ?>
 <header>
+
     <h1><a href="index.php">Fly Guys</a></h1>
     <nav>
         <?php if ($authenticated) : ?>
             <p><?= $customer->firstName ?></p>
         <?php endif ?>
-        <ul>
-            <li><a href="basket.php">Basket</a></li>
+        <div class="menu">
+            <div><a href="basket.php">Basket</a></div>
             <?php if ($authenticated) : ?>
-                <li><a href="user.php">Profile</a></li>
-                <li><a href="logout.php">Logout</a></li>
+                <div><a href="user.php">Profile</a></div>
+                <div><a href="logout.php">Logout</a></div>
             <?php else : ?>
-                <li><a href="login.php">Login</a></li><!-- Could be removed if we're going to keep the login form -->
+                <div><a href="login.php">Login</a></div><!-- Could be removed if we're going to keep the login form -->
             <?php endif ?>
-            <li><a href="register.php">Register</a></li>
+            <div><a href="register.php">Register</a></div>
         </ul>
+        </div>
     </nav>
+
 </header>
